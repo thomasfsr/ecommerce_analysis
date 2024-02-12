2f5cb7896f060f85b1477c5adaedc9c00ded0b35
Thomás Freire <thomas.fsr@gmail.com>
2024-02-12 12:06:46 -0300

    add packages to implement sql feeding to postgre

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-from src.main import create_dir, convert_to_parquet, export_to_s3
+from src.main import create_dir, convert_to_parquet, export_to_s3, export_to_postgre
 
 path = '/mnt/c/Users/home/Downloads/data'
 output = 'parquet_files'
